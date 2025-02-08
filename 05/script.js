51aeede7d6fc87e861727e8a0c377b01d561b0ac
Davincent1 <davincentlee008@gmail.com>
2025-02-08 10:07:31 +0700

    Delete 05 directory

--- 05/script.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function(){
-
-    var YearText = document.getElementById("YearText");
-    
-    YearText.innerHTML  = new Date().getFullYear();
-    
-})
