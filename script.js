070986171f82f9998a82d9e8e92bca04fa4e2e1e
Davincent1 <davincentlee008@gmail.com>
2025-02-08 10:08:31 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    var YearText = document.getElementById("YearText");
+    
+    YearText.innerHTML  = new Date().getFullYear();
+    
+})
